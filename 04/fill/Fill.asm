38c4c71d2f9a3c374e94fdf5670a1feec8b2e359
Luca Bolognese <lucabol@microsoft.com>
2023-03-13 08:58:36 +0100

    fill implemented

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,67 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+  @24576
+  D=A
+  @screenend
+  M=D
+
+  @color
+  M=-1
+
+(LOOP)
+  @SCREEN
+  D=A
+  @screenptr
+  M=D
+
+  // If no key is pressed goto white
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+
+  // Otherwise goto black
+  @BLACK
+  0;JMP
+
+(WHITE)
+  @color
+  M=0
+  @SCREENPRINT
+  0;JMP
+
+(BLACK)
+  @color
+  M=-1
+
+(SCREENPRINT)
+  // Get color (this should be outside the loop)
+  @color
+  D=M
+
+  // Dereference pointer to current screen location
+  @screenptr
+  A=M
+  M=D
+
+  // Increase pointer
+  @screenptr
+  M=M+1
+  
+  // If not end of screen continue printing
+  @screenptr
+  D=M
+  @screenend
+  D=D-M
+
+  @SCREENPRINT
+  D;JLT
+
+  // Else goto start
+  @LOOP
+  0;JMP
+
+
+
+
